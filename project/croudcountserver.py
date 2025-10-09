ee153edb51b67a383083f5c0e7ecfd47403844d4
shanmuk27 <shanmuksaisrinivas27@gmail.com>
2025-10-09 11:59:29 +0530

    submit form linked

--- project/croudcountserver.py
+++ project/croudcountserver.py
@@ -1,6 +1,24 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, session
+import pyrebase
+
+#for firebase
+config = {
+  'apiKey': "AIzaSyAHT27Lik4POA67GsBkeVUHWvWYeUqysi4",
+  'authDomain': "crowd-count-3e92c.firebaseapp.com",
+  'databaseURL': "https://crowd-count-3e92c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  'projectId': "crowd-count-3e92c",
+  'storageBucket': "crowd-count-3e92c.firebasestorage.app",
+  'messagingSenderId': "605062371417",
+  'appId': "1:605062371417:web:6e259e609153b20640f948",
+  'measurementId': "G-GXK3GQKMHG"
+}
+
+firebase=pyrebase.initialize_app(config)
+auth=firebase.auth()
+db=firebase.database()
 
 app = Flask(__name__)
+app.secret_key = 'a-very-secret-key'
 
 @app.route('/')
 def root():
@@ -8,32 +26,66 @@ def root():
 
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    if 'user' in session:
+        uid = session['user']['uid']
+        idTOken=session['user']['idToken']
+        user_data = db.child("user").child(uid).get(idTOken).val()
+        username = user_data.get('user_name', 'User')
+        return render_template('index.html', username=username)
+    else:
+        return redirect(url_for('signin'))
 
 @app.route('/signin')
 def signin():
     return render_template('signin.html')
 
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        email = request.form['email']
+        password = request.form['password']
+        user = auth.sign_in_with_email_and_password(email, password)
+        uid = user['localId']
+        session['user'] ={'uid': user['localId'],'idToken': user['idToken']}
+        return redirect(url_for('index'))
+    except Exception as e:
+        return render_template('signin.html', Error="Invalid username/password!!")
+
+@app.route('/register', methods=['POST'])
+def register():
+    try:
+        email = request.form['email']
+        password = request.form['password']
+        username = request.form['username']
+        
+        user = auth.create_user_with_email_and_password(email, password)
+        uid = user['localId']
+        id_token = user['idToken']
+        
+        data = {"user_name": username}
+        db.child("user").child(uid).set(data, id_token)
+
+        return redirect(url_for('signin'))
+    except Exception as e:
+        error = "Could not register. The email might already be in use."
+        return render_template('signin.html', Error=error,mode='register')
+
+@app.route('/profile')
+def profile():
+    if 'user' not in session:
+        return redirect(url_for('signin'))
+    return render_template('profile.html')
+
 @app.route('/signout')
 def signout():
-    # In a real app, you would clear the user's session here.
+    session.pop('user', None)
     return redirect(url_for('signin'))
 
 @app.route('/home')
 def home():
+    if 'user' not in session:
+        return redirect(url_for('signin'))
     return render_template('home.html')
-
-@app.route('/vid_analy')
-def vid_analy():
-    return render_template('vid_analy.html')
-
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
-
-@app.route('/cam_manage')
-def cam_manage():
-    return render_template('cam_manage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
